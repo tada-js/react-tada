47a379a1c475303c17db8f2a2c935bbb3ad13959
tada-js <justfix010@gmail.com>
2024-08-08 18:25:30 +0900

    :bulb: Add : tada.ts comments

--- src/lib/tada.ts
+++ src/lib/tada.ts
@@ -14,9 +14,30 @@ type Store<T> = {
   subscribe: (callback: () => void) => () => void;
 };
 
+/**
+ * Creates a Tada context and provider for managing state.
+ * @template T
+ * @param {T} initialState - The initial state for the store.
+ * @returns {Object} An object containing the TadaProvider component and useTada hook.
+ * @example
+ * import { tada } from 'react-tada';
+
+type State {
+  count: number;
+}
+
+export const useCountTada = tada<State>({ count: 0 });
+ */
 export const tada = <T>(initialState: T) => {
   const TadaContext = createContext<Store<T>>(createTada(initialState));
 
+  /**
+   * TadaProvider component that provides the Tada context to its children.
+   * @param {Object} props - The props for the TadaProvider component.
+   * @param {T} [props.initialState] - An optional custom initial state.
+   * @param {ReactNode} props.children - The child components that will have access to the Tada context.
+   * @returns {JSX.Element} The TadaProvider component.
+   */
   const TadaProvider = ({
     initialState: customInitialState,
     children,
@@ -37,7 +58,17 @@ export const tada = <T>(initialState: T) => {
     );
   };
 
+  /**
+   * A hook to use the Tada state and setState function.
+   * @returns {[T, (action: T | ((prev: T) => T)) => void]} An array containing the current state and the setState function.
+   */
   function useTada(): [T, Store<T>['setState']];
+  /**
+   * A hook to use a selected part of the Tada state and setState function.
+   * @template S
+   * @param {(state: T) => S} selector - A function to select a part of the state.
+   * @returns {[S, (action: T | ((prev: T) => T)) => void]} An array containing the selected state and the setState function.
+   */
   function useTada<S>(selector: (state: T) => S): [S, Store<T>['setState']];
 
   function useTada<S>(selector?: (state: T) => S) {
