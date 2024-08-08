53c0340fc73977b008535da4609ca7ea4827edc4
tada-js <justfix010@gmail.com>
2024-08-08 18:19:52 +0900

    :bulb: Add : createTada comments

--- src/lib/createTada.ts
+++ src/lib/createTada.ts
@@ -4,6 +4,12 @@ type Store<T> = {
   subscribe: (callback: () => void) => () => void;
 };
 
+/**
+ * @template T
+ * @param {T} initialState - The initial state of the store.
+ * @returns {Object} An object containing getState, setState, and subscribe functions.
+ * @description The createTada function takes an initial state and returns getState, setState, and subscribe functions.
+ */
 export const createTada = <T>(initialState: T): Store<T> => {
   let state = initialState;
   const callbacks = new Set<() => void>();
